b9102f13e285f9dff7ba24a316b6fabcf9f6035d
Alex Chan <alyxchan87@gmail.com>
2023-04-06 14:04:03 -0700

    Add vertex buffer and draw array calls - blank screen since we do not have shader code

--- OpenGL-Project/src/Application.cpp
+++ OpenGL-Project/src/Application.cpp
@@ -29,8 +29,23 @@ int main(void)
 
     // just sample code to ensure glew is linked
     std::cout << glGetString(GL_VERSION) << std::endl;
-    unsigned int a;
-    glGenBuffers(1, &a);
+    
+    
+    // this is a basic vertex buffer (1 vertex length = 4 bytes?)
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+
+    // this is like "selecting the buffer" in opengl this is called binding
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+
+    // specify the data of our buffer
+    float position[6] = {
+        -0.5f, -0.5f,
+        0.0f, 0.5f,
+        0.5f, -0.5f,
+    };
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), position, GL_STATIC_DRAW);
+
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -38,11 +53,15 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
+        //glBegin(GL_TRIANGLES);
+        //glVertex2f(-0.5f, -0.5f);
+        //glVertex2f(0.0f, 0.5f);
+        //glVertex2f(0.5f, -0.5f);
+        //glEnd();
+
+        // this function call knows what buffer to use because on line 39, we bound the buffer
+        // we could clear the buffer by calling glBindBuffer(GL_ARRAY_BUFFER, 0)
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
